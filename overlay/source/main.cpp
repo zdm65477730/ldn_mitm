855ccfd2c148ef34ee73fe9ce7fdff52a8447128
Damien Zhao <zdm65477730@126.com>
2025-08-16 09:31:35 +0800

    add multi-lang support
    Signed-off-by: Damien Zhao <zdm65477730@126.com>

--- overlay/source/main.cpp
+++ overlay/source/main.cpp
@@ -2,6 +2,8 @@
 #include <tesla.hpp>    // The Tesla Header
 #include "ldn.h"
 
+using namespace tsl;
+
 enum class State {
     Uninit,
     Error,
@@ -15,7 +17,7 @@ char g_version[32];
 
 class EnabledToggleListItem : public tsl::elm::ToggleListItem {
 public:
-    EnabledToggleListItem() : ToggleListItem("Enabled", false) {
+    EnabledToggleListItem() : ToggleListItem("EnabledToggleListItemText"_tr, false) {
         u32 enabled;
         Result rc;
 
@@ -37,7 +39,7 @@ public:
 
 class LoggingToggleListItem : public tsl::elm::ToggleListItem {
 public:
-    LoggingToggleListItem() : ToggleListItem("Logging", false) {
+    LoggingToggleListItem() : ToggleListItem("LoggingToggleListItemText"_tr, false) {
         u32 enabled;
         Result rc;
 
@@ -59,17 +61,33 @@ public:
 
 class MainGui : public tsl::Gui {
 public:
-    MainGui() { }
+    MainGui() {
+        std::string jsonStr = R"(
+            {
+			    "PluginName": "ldn_mitm",
+			    "NotLoadedErrorMainGuiListItemText": "ldn_mitm is not loaded.",
+			    "WrongStateErrorMainGuiListItemText": "wrong state",
+			    "LoggingToggleListItemText": "Logging",
+			    "EnabledToggleListItemText": "Enabled"
+            }
+        )";
+        std::string lanPath = std::string("sdmc:/switch/.overlays/lang/") + APPTITLE + "/";
+        fsdevMountSdmc();
+        tsl::hlp::doWithSmSession([&lanPath, &jsonStr]{
+            tsl::tr::InitTrans(lanPath, jsonStr);
+        });
+        fsdevUnmountDevice("sdmc");
+    }
 
     virtual tsl::elm::Element* createUI() override {
-        auto frame = new tsl::elm::OverlayFrame("ldn_mitm", g_version);
+        auto frame = new tsl::elm::OverlayFrame("PluginName"_tr, g_version);
 
         auto list = new tsl::elm::List();
 
         if (g_state == State::Error) {
-            list->addItem(new tsl::elm::ListItem("ldn_mitm is not loaded."));
+            list->addItem(new tsl::elm::ListItem("NotLoadedErrorMainGuiListItemText"_tr));
         } else if (g_state == State::Uninit) {
-            list->addItem(new tsl::elm::ListItem("wrong state"));
+            list->addItem(new tsl::elm::ListItem("WrongStateErrorMainGuiListItemText"_tr));
         } else {
             list->addItem(new EnabledToggleListItem());
             list->addItem(new LoggingToggleListItem());
@@ -91,7 +109,7 @@ public:
     }
 };
 
-class Overlay : public tsl::Overlay {
+class LdnmitmOverlay : public tsl::Overlay {
 public:
     virtual void initServices() override {
         g_state = State::Uninit;
@@ -133,5 +151,5 @@ public:
 };
 
 int main(int argc, char **argv) {
-    return tsl::loop<Overlay>(argc, argv);
+    return tsl::loop<LdnmitmOverlay>(argc, argv);
 }
